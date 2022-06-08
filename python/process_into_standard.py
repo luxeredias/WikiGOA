cef56dfc182eba868b5951f3aa6a3cd9ca094ba2
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-06-08 10:32:10 -0300

    feat: Add dataset in GMT

---
+++ python/process_into_standard.py
@@ -0,0 +1,20 @@
+import pandas as pd 
+from pathlib import Path
+
+HERE = Path(__file__).parent.resolve()
+
+df  = pd.read_csv(HERE.parent.joinpath("data/wikidata_GO_terms.tsv"), sep="\t")
+
+df = df[["sitelink", "itemLabel", "gene_symbol"]]
+
+df = df.drop_duplicates()
+
+full_gmt = ""
+for i in df.groupby("itemLabel"):
+  itemLabel = i[0]
+  sitelink = i[1]["sitelink"].values[0]
+  genes = i[1]["gene_symbol"].values
+  full_gmt += f"""
+{itemLabel}	{sitelink}	{"	".join(genes)}"""
+
+HERE.parent.joinpath("datasets/wikigoa_gene_symbol.gmt").write_text(full_gmt)
